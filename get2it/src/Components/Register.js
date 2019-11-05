6031fd4a8c854efb621e5068f20e2874831e7aff
teej387 <teej387@yahoo.com>
2019-11-04 22:28:37 -0600

    Cleaned up code

--- get2it/src/Components/Register.js
+++ get2it/src/Components/Register.js
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
-
-// const Register = (props) => {
-//   return (
-//     <Form>
-//       <FormGroup className='formGroup' row>
-//         <Col sm='2'>
-//           <Label for="username"><i className="far fa-user-circle"></i></Label>
-//         </Col>
-//         <Col sm='auto'>
-//           <Input type="text" name="username" id="username" placeholder="Username" />
-//         </Col>
-//       </FormGroup>
-//       <FormGroup className='formGroup' row>
-//         <Col sm={2}>
-//           <Label for="examplePassword"><i className="fas fa-unlock-alt"></i></Label>
-//         </Col>
-//         <Col sm='auto'>
-//           <Input type="password" name="password" id="examplePassword" placeholder="Password" />
-//         </Col>
-//       </FormGroup>
-//       <Button>Continue</Button>
-//     </Form>
-//   )
-// }
 import React from 'react'
 import { withRouter, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
