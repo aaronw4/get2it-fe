225db28c740fda29874432020f7255b73d239ff9
teej387 <teej387@yahoo.com>
2019-11-05 22:43:08 -0600

    Created Login component

--- get2it/src/Components/App.js
+++ get2it/src/Components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import '../App.css';
 import Register from './Register/Register.js'
+import Login from './Login/Login.js'
 import OnBoarding from './OnBoarding'
 
 function App() {
   return (
     <div className="App">
-      <Register />
-      <div>
-        <OnBoarding />
-      </div>
+      {/* <Register /> */}
+      <Login />
+      {/* <OnBoarding /> */}
     </div>
   );
 }
