bc691398502836193e9a2c07142051513a7c9dbb
teej387 <teej387@yahoo.com>
2019-11-06 19:42:45 -0600

    Imported react-router and any needed Components

--- get2it/src/Components/App.js
+++ get2it/src/Components/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../App.css';
+import { Route, withRouter } from 'react-router-dom'
+// import { connect } from 'react-redux'
+import PrivateRoute from './PrivateRoute/PrivateRoute.js'
+import Dashboard from './Dashboard/Dashboard.js'
 import Register from './Register/Register.js'
 import Login from './Login/Login.js'
 import OnBoarding from './OnBoarding'
@@ -7,9 +11,7 @@ import OnBoarding from './OnBoarding'
 function App() {
   return (
     <div className="App">
-      {/* <Register /> */}
-      <Login />
-      {/* <OnBoarding /> */}
+      
     </div>
   );
 }
