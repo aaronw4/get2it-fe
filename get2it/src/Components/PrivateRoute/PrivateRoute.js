8e3a84469ebd0dc91a669db1bd38f102e0311837
teej387 <teej387@yahoo.com>
2019-11-06 18:47:31 -0600

    Created PrivateRoute component

---
+++ get2it/src/Components/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+function PrivateRoute(props) {
+  const {
+    component: Component,
+    ...rest
+  } = props
+
+  return (
+
+    <Route {...rest} render={(props) => {
+
+      const token = localStorage.getItem('token')
+
+      return token
+        ? <Component {...props} />
+        : <Redirect to="/login" />
+    }} />
+  )
+}
+
+export default PrivateRoute
