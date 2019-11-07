7546acfa19078a9f14be618bfb897a60288b0952
teej387 <teej387@yahoo.com>
2019-11-06 19:32:46 -0600

    Created Dashboard for displaying authenticated routes

--- get2it/src/Components/Dashboard/Dashboard.js
+++ get2it/src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,35 @@
 import React from 'react';
+import './Dashboard.css'
 import { connect } from 'react-redux'
 import { Route, withRouter } from 'react-router-dom'
 
+class Dashboard extends React.Component {
+
+  componentDidMount() {
+    this.props.getAccount()
+  }
+
+  render() {
+
+    return (
+
+      <div className="dashboard">
+
+        {this.props.isLoading ? <p className='loading'>Loading...</p> :
+            <div className='appRoutes'>
+              <p>TEMP MESSAGE (remove later)</p>
+              {/* <Route exact path='/' render={props => <Home {...props} />} /> */}
+
+            </div>
+        }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  // userData: state.userData,
+  isLoading: state.isLoading,
+})
+
+export default withRouter(connect(mapStateToProps)(Dashboard));
