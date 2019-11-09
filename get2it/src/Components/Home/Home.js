ab3bc6a7fcafaa1d9b5c93b9f4c8ce95d00718f4
teej387 <teej387@yahoo.com>
2019-11-08 19:47:40 -0600

    Created a condition for the greeting

--- get2it/src/Components/Home/Home.js
+++ get2it/src/Components/Home/Home.js
@@ -5,10 +5,13 @@ import { Link, Route } from 'react-router-dom'
 import moment from 'moment'
 
 function Home(props) {
-  console.log(moment().format('LT'))
+  const time = moment().format('H')
+  console.log(time <= 19)
 
   return (
     <div className='home'>
+      {time > 4 && time < 11 ? <h2 className='greeting'>Good morning</h2> 
+        : time >= 11 && time < 4 ? <h2 className='greeting'>Good afternoon</h2> : <h2 className='greeting'>Good evening</h2>}
       <div className='today'>
         <p className='date'>{moment().format('LL')}</p>
         <p className='time'>{moment().format('LT')}</p>
