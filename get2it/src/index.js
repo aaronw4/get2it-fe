d553a5574e80c554a25d57e6f83d179454a7edf2
teej387 <teej387@yahoo.com>
2019-11-12 16:31:17 -0600

    Set to persist only userData

--- get2it/src/index.js
+++ get2it/src/index.js
@@ -16,6 +16,7 @@ import reducer from './reducer'
 const persistConfig = {
   key: 'root',
   storage: storage,
+  whitelist: ['userData']
 }
 
 const persistedReducer = persistReducer(persistConfig, reducer)
