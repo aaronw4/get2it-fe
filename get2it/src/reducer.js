e570f044c7486b0ef5e41c65295a297ac92b643e
teej387 <teej387@yahoo.com>
2019-11-14 17:34:28 -0600

    Fixed css bug

--- get2it/src/reducer.js
+++ get2it/src/reducer.js
@@ -20,7 +20,7 @@ import {
 const dummyTasks = [
   {
     name: 'Go to the grocery store.',
-    date: '11/14/2019',
+    date: '11/13/2019',
     task_icon: '<i id="icon" className="fas fa-shopping-cart icon"></i>',
     start_time: '9am',
     end_time: '11am',
@@ -28,7 +28,7 @@ const dummyTasks = [
   },
   {
     name: 'Work out.',
-    date: '11/14/2019',
+    date: '11/13/2019',
     task_icon: '<i id="icon" className="fas fa-heartbeat icon"></i>',
     start_time: '1pm',
     end_time: '2pm',
