cb14dff6ee27f67777f01bd377f74558db706f53
teej387 <teej387@yahoo.com>
2019-11-14 00:16:59 -0600

    styled the instructions

--- get2it/src/Components/Home/Home.js
+++ get2it/src/Components/Home/Home.js
@@ -23,50 +23,71 @@ class Home extends React.Component {
   render() {
     const {time, todayList} = this
     return (
-      <div className='home'>
-        {time >= 4 && time < 11 ? <h2 className='greeting'>Good morning</h2> 
-          : time >= 11 && time < 16 ? <h2 className='greeting'>Good afternoon</h2> : <h2 className='greeting'>Good evening</h2>}
-        <div className='today'>
-          <p className='date'>{moment().format('LL')}</p>
-          <p className='time'>
-            <Moment interval={10000} format='LT' />
+      <div className="home">
+        {time >= 4 && time < 11 ? (
+          <h2 className="greeting">Good morning</h2>
+        ) : time >= 11 && time < 16 ? (
+          <h2 className="greeting">Good afternoon</h2>
+        ) : (
+          <h2 className="greeting">Good evening</h2>
+        )}
+        <div className="today">
+          <p className="date">{moment().format("LL")}</p>
+          <p className="time">
+            <Moment interval={10000} format="LT" />
           </p>
         </div>
-        <Link className='countLink' to='/taskList'>
-          <div className='countContainer'>
-            <h1 className='count'>{todayList.length}</h1>
-            <p className='total'>{this.props.userTasks.length}</p>
+        <Link className="countLink" to="/taskList">
+          <div className="countContainer">
+            <h1 className="count">{todayList.length}</h1>
+            <p className="total">{this.props.userTasks.length}</p>
           </div>
         </Link>
-        <div className='homeList'>
-          {
-            todayList.length === 0 ?
-              <div className='noTaskContainer'>
-                <i className="fas fa-long-arrow-alt-up upArrow"></i>
-                <p className='instruction'>Click the counter to see all of your tasks!</p>
-                <img className='bigLogo' src={logo} alt='Get2It!' />
-                <p className='instruction'>Or add a new task!</p>
-              </div> :
+        <div className="homeList">
+          {todayList.length === 0 ? (
+            <div className="noTaskContainer">
+              <i className="fas fa-long-arrow-alt-up arrow"></i>
+              <p className="instruction">
+                See all of your tasks!
+              </p>
+              <div className="bigLogoContainer">
+                <img className="bigLogo" src={logo} alt="Get2It!" />
+              </div>
+              <p className="instruction">Or add a new task!</p>
+              <i className="fas fa-long-arrow-alt-down arrow"></i>
+            </div>
+          ) : (
             todayList.map((task, index) => {
               return (
-                <div className='listItem' key={index}>
-                  <div className='iconContainer'><JsxParser jsx={task.task_icon} /></div>
-                  <div className='itemContainer'>
-                    <p className='itemName'>{task.name}</p>
-                    <p className='duration'>{task.start_time}-{task.end_time}</p>
+                <div className="listItem" key={index}>
+                  <div className="iconContainer">
+                    <JsxParser jsx={task.task_icon} />
+                  </div>
+                  <div className="itemContainer">
+                    <p className="itemName">{task.name}</p>
+                    <p className="duration">
+                      {task.start_time}-{task.end_time}
+                    </p>
                   </div>
                 </div>
-              )
+              );
             })
-          }
+          )}
         </div>
-        
-        <Link className='addTaskLink' to={{pathname:'/taskModal', state: {modal: true}}}>+</Link>
-  
-        <Route path='/taskModal' render={props => <NewTaskModal {...props} />} />
-  
+
+        <Link
+          className="addTaskLink"
+          to={{ pathname: "/taskModal", state: { modal: true } }}
+        >
+          +
+        </Link>
+
+        <Route
+          path="/taskModal"
+          render={props => <NewTaskModal {...props} />}
+        />
       </div>
-    )
+    );
   }
 }
 
