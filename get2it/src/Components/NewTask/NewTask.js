f5d47fb1ac989069a401183d0641fed5ad55072f
slaforsjov <andrewj12763@gmail.com>
2019-11-20 22:17:59 -0700

    adding drop down to add task page

--- get2it/src/Components/NewTask/NewTask.js
+++ get2it/src/Components/NewTask/NewTask.js
@@ -6,14 +6,39 @@ import Date from "./Date";
 import EndTime from "./EndTime";
 import Label from "./Label";
 import Category from "./Category";
-import Dropdown from 'react-bootstrap'
-
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import { set } from "date-fns";
+import $ from 'jquery';
+import JsxParser from 'react-jsx-parser';
 
 class NewTask extends React.Component {
   constructor(props) {
     super(props);
-
-
+    this.state= {icon:""
+    }
+   
+  }
+  removeicon = () => {
+    // console.log('removing icon')
+    $('.icon').remove();
+    this.setState({
+      icon: ""
+    })
+  }
+  saveIcon= event => {
+    this.removeicon()
+    // var icontag = $('.addIcon').html();
+    // // console.log(icontag)
+    // var iconfix = icontag.replace('<svg id="icon" class="svg-inline--fa fa-heartbeat fa-w-16 iconDropdown" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="heartbeat" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M320.2 243.8l-49.7 99.4c-6 12.1-23.4 11.7-28.9-.6l-56.9-126.3-30 71.7H60.6l182.5 186.5c7.1 7.3 18.6 7.3 25.7 0L451.4 288H342.3l-22.1-44.2zM473.7 73.9l-2.4-2.5c-51.5-52.6-135.8-52.6-187.4 0L256 100l-27.9-28.5c-51.5-52.7-135.9-52.7-187.4 0l-2.4 2.4C-10.4 123.7-12.5 203 31 256h102.4l35.9-86.2c5.4-12.9 23.6-13.2 29.4-.4l58.2 129.3 49-97.9c5.9-11.8 22.7-11.8 28.6 0l27.6 55.2H481c43.5-53 41.4-132.3-7.3-182.1z"></path></svg><!-- <i id="icon" class="', '');
+    // console.log(iconfix)
+    // var iconfixtwo = iconfix.replace(' -->', '')
+    // console.log(iconfixtwo)
+    // var iconclass= '<i id="icon" class="icon '
+    // var iconfixthree = iconclass.concat(iconfixtwo)
+    // console.log(iconfixthree)
+    this.setState({
+      icon: '<i id="icon" className="fas fa-heartbeat iconDropdown"></i>'
+    })
   }
 
   render() {
@@ -40,20 +65,39 @@ class NewTask extends React.Component {
         <hr className="line" />
 
         <EndTime />
-        <br />
 
         <div className="app">
           
-          <br />
-          {/* <ToDoTask toDoTasks={this.state.toDoTasks} deleteItem={this.deleteItem.bind(this)} saveItem={this.saveItem.bind(this)} toggleComplete={this.toggleComplete.bind(this)} /> */}
         </div>
-        <br />
         <hr className="line" />
-        <Label />
+        <form>
+          <label className="newTaskLableName">
+            New Task Name:
+            <input className="newTaskInput" type="text" name="name" />
+          </label>
+        </form>
+
         <hr className="line" />
+        <DropdownButton id="dropdown-item-button">
+          <Dropdown.Item className="addIcon" onClick={this.saveIcon} as="button">
+            <i id="icon" className="fas fa-heartbeat iconDropdown"></i>
+          </Dropdown.Item>
+          <Dropdown.Item className="addIcon" onClick={this.saveIcon} as="button">
+            <i id="icon" className="fas fa-heartbeat iconDropdown"></i>
+          </Dropdown.Item>
+          <Dropdown.Item className="addIcon" onClick={this.saveIcon} as="button">
+            <i id="icon" className="fas fa-heartbeat iconDropdown"></i>
+          </Dropdown.Item>
+        </DropdownButton>
+        {/* <Label /> */}
+        <div><JsxParser jsx={this.state.icon} /></div>
+        <hr className="line" />
+        
       </div>
     );
   }
 }
 
 export default NewTask;
+
+
