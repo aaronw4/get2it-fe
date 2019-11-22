57776e6744df56cc9ae9c2392805ebe72d6536fc
teej387 <teej387@yahoo.com>
2019-11-21 19:29:26 -0600

    Converted to class component and saved state to redux

--- get2it/src/Components/NewTask/Date.js
+++ get2it/src/Components/NewTask/Date.js
@@ -4,36 +4,63 @@ import Kalendaryo from "kalendaryo";
 import Calendar from "./Calender";
 import { format } from "date-fns";
 import "./NewTask.css";
+import { newTaskDate } from '../../actions.js'
+import { connect } from 'react-redux'
 
-function Date(props) {
-  return (
-    <Downshift
-      itemToString={date => (date ?  console.log(format(date, 'MM/dd/yyyy')) : "")}
-      
-      {...props}
+class Date extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: '',
+    }
+  }
+  render() {
+    console.log(this.state.date)
+
+    return (
+      <Downshift
+        id="date"
+        itemToString={date => (date ? format(date, "MM/dd/yyyy") : "")}
+        onSelect={date => {
+          const newDate = format(date, "MM/dd/yyyy");
+          this.setState({ date: newDate });
+          newTaskDate(this.state.date)
+        }}
+        {...this.props}
       >
-      {({ getInputProps, getItemProps, toggleMenu, isOpen, selectedItem }) => (
-        <div className="date-picker-container">
-        
-          <input 
-            {...getInputProps({
-              readOnly: true,
-              placeholder: "Date",
-              onClick: toggleMenu
-            })}
-          />
-          
-          {isOpen ? (
-            <Kalendaryo
-              startCurrentDateAt={selectedItem}
-              selectedItem={selectedItem}
-              getItemProps={getItemProps}
-              render={Calendar}
+        {({
+          getInputProps,
+          getItemProps,
+          toggleMenu,
+          isOpen,
+          selectedItem
+        }) => (
+          <div className="date-picker-container">
+            <input
+              {...getInputProps({
+                readOnly: true,
+                placeholder: "Date",
+                onClick: toggleMenu
+              })}
             />
-          ) : null}
-        </div>
-      )}
-    </Downshift>
-  );
+
+            {isOpen ? (
+              <Kalendaryo
+                startCurrentDateAt={selectedItem}
+                selectedItem={selectedItem}
+                getItemProps={getItemProps}
+                render={Calendar}
+              />
+            ) : null}
+          </div>
+        )}
+      </Downshift>
+    );
+  }
 }
-export default Date;
+
+const mapDispatchToProps = {
+  newTaskDate,
+};
+
+export default connect(null,mapDispatchToProps)(Date);
