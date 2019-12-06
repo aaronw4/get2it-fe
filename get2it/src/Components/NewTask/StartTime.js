c6227eee8bee0e1dd19ee458d915a01f89137e96
slaforsjov <andrewj12763@gmail.com>
2019-12-05 18:24:12 -0700

    merge

--- get2it/src/Components/NewTask/StartTime.js
+++ get2it/src/Components/NewTask/StartTime.js
@@ -1,35 +1,34 @@
 import React, { Component } from "react";
 import moment from "moment";
 import TimePicker from "./TimePicker";
-import './NewTask.css'
-import { newStartTime } from '../../actions.js'
-import { connect } from 'react-redux'
+import "./NewTask.css";
+import { newStartTime } from "../../actions.js";
+import { connect } from "react-redux";
 
 class TimeSelectForm extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      start_time: moment(),
-    }
+      start_time: moment()
+    };
   }
   handleChange = value => {
-    this.setState({ start_time: value })
+    this.setState({ start_time: value });
     this.props.newStartTime(moment(this.state.start_time).format("h:mm a"));
   };
-  
+
   handleSubmit = e => {
     e.preventDefault();
     alert(moment(this.state.start_time).format("hh:mm a"));
   };
-  
-  render = (props) => {
-    // console.log(moment(this.state.start_time).format("h:mm a"));
-    return(
 
+  render = props => {
+    // console.log(moment(this.state.start_time).format("h:mm a"));
+    return (
       <div>
         <h1 className="Title2" > START</h1>
         <div className="startTime">
-        
+
           <form onSubmit={this.handleSubmit}>
             <i className="far fa-clock fa-3x"></i>
             <TimePicker value={this.state.start_time} onChange={this.handleChange} />
@@ -37,12 +36,24 @@ class TimeSelectForm extends Component {
           </form>
         </div>
       </div>
-    )
+      // <div className="startCont">
+      //   <i id="startClock" className="fas fa-clock "></i>
+      //   <h3 className="startTitle">START</h3>
+      //   <input
+      //     className="inputClock"
+      //     value={this.state.start_time}
+      //     onChange={this.handleChange}
+      //     type="time"
+          
+          
+      //   ></input>
+      // </div>
+    );
   };
 }
 
 const mapDispatchToProps = {
-  newStartTime,
+  newStartTime
 };
 
-export default connect(null,mapDispatchToProps)(TimeSelectForm);
+export default connect(null, mapDispatchToProps)(TimeSelectForm);
