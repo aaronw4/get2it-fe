96810d382b1cad9ba38383c503f825c261dd64f9
teej387 <teej387@yahoo.com>
2019-12-17 20:38:38 -0600

    Creating Alert in dashboard

--- get2it/src/Components/Dashboard/Dashboard.js
+++ get2it/src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Dashboard.css'
+import Alert from 'react-bootstrap/Alert'
 import { connect } from 'react-redux'
 import { Route, withRouter } from 'react-router-dom'
 import OnBoarding from '../OnBoarding/index.js'
@@ -23,29 +24,48 @@ class Dashboard extends React.Component {
   render() {
     return (
       <>
-        {this.props.isLoading ? <Spinner /> :
-          <div className="dashboard">
-            <div className="nav">
-              <Menu />
-            </div>
-            <div className="appRoutes">
-              <Route
-                path="/onboarding"
-                render={props => <OnBoarding {...props} />}
-              />
+        {() => {
+          if(this.props.isLoading) {
+            return (
+              <Spinner />
+            )
+          }else if (this.props.errorStatus === 401) {
+            return (
+              <Alert variant="danger" onClose={() => this.props.history.push('/login')} dismissible>
+                <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+                <p>
+                  Change this and that and try again. Duis mollis, est non commodo
+                  luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
+                  Cras mattis consectetur purus sit amet fermentum.
+                </p>
+              </Alert>
+            )
+          }else {
+            return (
+              <div className="dashboard">
+              <div className="nav">
+                <Menu />
+              </div>
+              <div className="appRoutes">
+                <Route
+                  path="/onboarding"
+                  render={props => <OnBoarding {...props} />}
+                />
 
-              <Route exact path="/" render={props => <Home {...props} />} />
-              <Route path="/NewTask" render={props => <NewTask {...props} />} />
-              <Route path="/taskList" render={props => <TaskList {...props} />} />
-              <Route path="/taskModal" render={props => <Home {...props} />} />
-              <Route path="/profile" render={props => <Profile {...props} />} />
-              <Route path="/CompletedTaskList" render={props => <CompletedTaskList {...props} />} />
-              <Route path="/edittaskModal" render={props => <TaskList {...props} />} />
-              <Route path="/EditTaskList" render={props => <EditTaskList {...props} />} />
+                <Route exact path="/" render={props => <Home {...props} />} />
+                <Route path="/NewTask" render={props => <NewTask {...props} />} />
+                <Route path="/taskList" render={props => <TaskList {...props} />} />
+                <Route path="/taskModal" render={props => <Home {...props} />} />
+                <Route path="/profile" render={props => <Profile {...props} />} />
+                <Route path="/CompletedTaskList" render={props => <CompletedTaskList {...props} />} />
+                <Route path="/edittaskModal" render={props => <TaskList {...props} />} />
+                <Route path="/EditTaskList" render={props => <EditTaskList {...props} />} />
 
+              </div>
             </div>
-          </div>
-        }
+            )
+          }
+        }}
       </>
     );
   }
@@ -55,6 +75,7 @@ const mapStateToProps = (state) => ({
   userTasks: state.userTasks,
   userData: state.userData,
   isLoading: state.isLoading,
+  errorStatus: state.errorStatus,
 })
 
 const mapDispatchToProps = {
