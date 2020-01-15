dc87043ed632a45099409710038a9394f7549a05
teej387 <teej387@yahoo.com>
2020-01-15 15:12:36 -0600

    Created event for showing notifications

--- get2it/src/Components/Notifications/Notification.js
+++ get2it/src/Components/Notifications/Notification.js
@@ -4,14 +4,37 @@ import './notification.css'
 
 
 export default class Notification extends React.Component {
-  
+  constructor(props) {
+    super(props)
+    this.state = {
+      top: -100,
+    }
+  }
 
+  onShow = () => {
+
+  }
+
+  showNotification = () => {
+    this.setState({
+      top: 16,
+    }, () => {
+      setTimeout(() => {
+        this.setState({
+          top: -100,
+        })
+      }, 5000);
+    })
+  }
 
   render() {
     return (
-      <div className='notificationContainer'>
-        <p className='notificationBody'>You've been notified!</p>
-      </div>
+      <>
+        <button onClick={this.showNotification} style={{}}>Hey!</button>
+        <div className='notificationContainer' style={{top: this.state.top}}>
+          <p className='notificationBody'>You've been notified!</p>
+        </div>
+      </>
     )
   }
 
