191563e777e9e5f23786f0fe95ef27b649027426
Aaron Williams <aaronrw4@yahoo.com>
2020-04-24 20:17:04 -0700

    Created a testing function for redux

---
+++ get2it/src/Testing-renderWithRedux.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {render} from '@testing-library/react';
+import {initialState as reducerInitialState, reducer} from './reducer';
+
+function renderWithRedux(
+    component, 
+    {
+      initialState = reducerInitialState, 
+      store = createStore(reducer, initialState),
+      ...renderOptions
+    } = {}
+  ) {
+    function Wrapper({component}) {
+      return <Provider store={store}>{component}</Provider>
+    }  
+    return render(component, {wrapper: Wrapper, ...renderOptions})
+  }
+
+export default renderWithRedux;
