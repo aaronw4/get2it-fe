31d14d90fc854b2f841a0c4805e16f201e3734c9
Aaron Williams <aaronrw4@yahoo.com>
2020-04-28 18:16:00 -0700

    updated tests

--- get2it/src/Components/Login/Login.test.js
+++ get2it/src/Components/Login/Login.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import {renderWithRedux, cleanup, fireEvent} from '../../test-utils';
+import {renderWithRedux, cleanup, fireEvent, screen} from '../../test-utils';
 import Login from './Login';
-import { waitForDomChange } from '@testing-library/react';
 
 afterEach(cleanup);
 
@@ -10,12 +9,7 @@ it('renders without crashing', () => {
   renderWithRedux(<Login/>);
 });
 
-it('displays header', () => {
-  const container = renderWithRedux(<Login/>);
-  waitForDomChange({container})
-    .then(() => {
-      const element = container.getByTestId('loginHeader');
-      expect(element).toBeVisible();
-    })
-    .catch(err => console.log(err))
-});
+it('displays header2', ()  => {
+  renderWithRedux(<Login/>);
+  screen.getByText('Sign In');
+})
