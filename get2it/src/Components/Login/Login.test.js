146c1133769f264096e276cb58375e1bf734d9fa
Aaron Williams <aaronrw4@yahoo.com>
2020-04-28 22:49:35 -0700

    Wrote tests for show password button

--- get2it/src/Components/Login/Login.test.js
+++ get2it/src/Components/Login/Login.test.js
@@ -5,11 +5,50 @@ import Login from './Login';
 
 afterEach(cleanup);
 
-it('renders without crashing', () => {
-  renderWithRedux(<Login/>);
-});
-
-it('displays header2', ()  => {
+it('displays header', ()  => {
   renderWithRedux(<Login/>);
   screen.getByText('Sign In');
-})
+});
+
+it('inputs email', () => {
+  const {getByPlaceholderText} = renderWithRedux(<Login/>)
+  const email = getByPlaceholderText(/email/i)
+  fireEvent.change(email, {target: {value: 'Aaron'}})
+  expect(email.value).toBe('Aaron')
+});
+
+it('inputs password', () => {
+  const {getByPlaceholderText} = renderWithRedux(<Login/>)
+  const password = getByPlaceholderText(/password/i)
+  fireEvent.change(password, {target: {value: 'pass'}})
+  expect(password.value).toBe('pass')
+});
+
+it('password has type password', () => {
+  const {getByPlaceholderText} = renderWithRedux(<Login/>)
+  const password = getByPlaceholderText(/password/i)
+  expect(password).toHaveAttribute('type','password')
+});
+
+it('click on Show Password, button has type text', () => {
+  const {getByPlaceholderText, getByText} = renderWithRedux(<Login/>)
+  const password = getByPlaceholderText(/password/i)
+  const button = getByText(/Show Password/i)
+  fireEvent.click(button)
+  expect(password).toHaveAttribute('type','text')
+});
+
+it('click on Show Password then click on Hide Password, button has type password', () => {
+  const {getByPlaceholderText, getByText} = renderWithRedux(<Login/>)
+  const password = getByPlaceholderText(/password/i)
+  const button = getByText(/Show Password/i)
+  fireEvent.click(button)
+  fireEvent.click(button)
+  expect(password).toHaveAttribute('type','password')
+});
+
+it('has a link to register', () => {
+  const {getByText} = renderWithRedux(<Login/>)
+  const link = getByText(/SIGN UP/i)
+  expect(link).toHaveAttribute('href', '/register')
+});
