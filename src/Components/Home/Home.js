006abf67b5158625bfcecb783740aebb5c6fd179
CodingCCarpenter <Christineassists@aol.com>
2020-06-15 18:23:51 -0500

    cleaning up unused code and unnessesary imports

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import JsxParser from 'react-jsx-parser';
 import './Home.css';
 import 'react-router-modal/css/react-router-modal.css';
 import { connect } from 'react-redux';
 import { Link, Route, withRouter } from 'react-router-dom';
 import moment from 'moment';
-import Moment from 'react-moment';
 import NewTaskModal from './NewTaskModal.js';
-import logo from '../Images/logo.png';
 import Filter from './Filter';
 import {updateFilteredTask} from '../../actions';
-import Timer from '../Timer/Timer/Timer';
 
 class Home extends React.Component {
   constructor(props) {
