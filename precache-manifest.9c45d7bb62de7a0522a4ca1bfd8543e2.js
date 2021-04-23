4be4de67666756f9200d4ab10f28e673c1842146
Aaron Williams <aaronrw4@yahoo.com>
2021-04-22 21:30:20 -0700

    Updates

---
+++ precache-manifest.9c45d7bb62de7a0522a4ca1bfd8543e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673115cef00125217eba28340939ad34",
+    "url": "/get2it-fe/index.html"
+  },
+  {
+    "revision": "c6b14f8d320092372dfe",
+    "url": "/get2it-fe/static/css/2.fcdd55ed.chunk.css"
+  },
+  {
+    "revision": "a2e2b5118b9b86bb1442",
+    "url": "/get2it-fe/static/css/main.b941c30b.chunk.css"
+  },
+  {
+    "revision": "c6b14f8d320092372dfe",
+    "url": "/get2it-fe/static/js/2.85af1be0.chunk.js"
+  },
+  {
+    "revision": "fbc350be159163350282558f277fed4e",
+    "url": "/get2it-fe/static/js/2.85af1be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2e2b5118b9b86bb1442",
+    "url": "/get2it-fe/static/js/main.63b30e12.chunk.js"
+  },
+  {
+    "revision": "39d9f8406906d662f69a",
+    "url": "/get2it-fe/static/js/runtime-main.c80e065a.js"
+  },
+  {
+    "revision": "3a122faa11f43ed659c3d5dac6f6476c",
+    "url": "/get2it-fe/static/media/focused.3a122faa.PNG"
+  },
+  {
+    "revision": "6bef7e8c4670fe1f8c5f675bb226eaa3",
+    "url": "/get2it-fe/static/media/fun.6bef7e8c.PNG"
+  },
+  {
+    "revision": "4b7c2aa8c8f453b97e78a8ce82ca8e80",
+    "url": "/get2it-fe/static/media/logo.4b7c2aa8.png"
+  },
+  {
+    "revision": "24a41e9f787872d17aeb0abe45394fee",
+    "url": "/get2it-fe/static/media/organized.24a41e9f.PNG"
+  }
+]);
